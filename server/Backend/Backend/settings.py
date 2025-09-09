316ac7ad7d23e65332dcb0a0eeacde5385e3a31d
Andre-Ovien <andreovien571@gmail.com>
2025-09-09 17:47:11 +0100

    minor tweaks

--- server/Backend/Backend/settings.py
+++ server/Backend/Backend/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(microseconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
