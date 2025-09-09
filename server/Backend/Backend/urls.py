a669fa9a22e0eaeb9da7bb13c3cf45e6d048a90b
Andre-Ovien <andreovien571@gmail.com>
2025-09-09 17:48:10 +0100

    minor changes

--- server/Backend/Backend/urls.py
+++ server/Backend/Backend/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import CreateUserView
+#from core.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('silk/', include('silk.urls', namespace='silk')),
-    path('api/user/register/', CreateUserView.as_view(), name="register"),
+#    path('api/user/register/', CreateUserView.as_view(), name="register"),
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
-    path('api/auth/', include('rest_framework.urls')),
+#    path('api/auth/', include('rest_framework.urls')),
 ]
