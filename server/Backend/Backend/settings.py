36a6b5fe3f1502e109b5df8da81f78d6038437c8
Andre-Ovien <andreovien571@gmail.com>
2025-09-14 07:32:08 +0100

    filter backends setup

--- server/Backend/Backend/settings.py
+++ server/Backend/Backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'silk',
     'corsheaders',
     'drf_spectacular',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -141,6 +142,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 SIMPLE_JWT = {
