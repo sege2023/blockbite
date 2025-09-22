628c8221888edef38317ad534b1f054df16380de
Andre-Ovien <andreovien571@gmail.com>
2025-09-22 17:53:17 +0100

    adjusted models for User

--- server/Backend/core/models.py
+++ server/Backend/core/models.py
@@ -5,16 +5,33 @@ import uuid
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+#   def create_user(self, email, password, **extra_fields):
+#        if not email:
+#            raise ValueError("The Email field must be set")
+#        email = self.normalize_email(email)
+#        user = self.model(email=email, **extra_fields)
+#        user.set_password(password)
+#        user.save(using=self._db)
+#        return user
+
+    def create_user(self, email, wallet_address=None, password=None, **extra_fields):
         if not email:
-            raise ValueError("The Email field must be set")
+            raise ValueError("Email is required")
+        if not wallet_address:
+            raise ValueError("Wallet address is required")
+    
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
+        user = self.model(email=email, wallet_address=wallet_address, **extra_fields)
+
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
@@ -23,7 +40,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Superuser must be a staff')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Must be a Superuser')
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, wallet_address='superuser', password=password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True, null=False)
