ee223807d1f7d366c963518daba08325441f6e82
Lah-deh <successasokere@gmail.com>
2025-09-25 18:11:50 +0100

    added few things

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -69,7 +69,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Login />} />
-              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
 
               <Route path="/signup" element={<Signup />} />
 
