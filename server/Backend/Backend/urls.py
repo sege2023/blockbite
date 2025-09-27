5eb12122382933261d66535597d0ecec3c134b38
Andre-Ovien <andreovien571@gmail.com>
2025-09-27 16:39:34 +0100

    update

--- server/Backend/Backend/urls.py
+++ server/Backend/Backend/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 #from core.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
@@ -15,4 +17,5 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name="swagger-ui"),    
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name="redoc"),    
 #    path('api/auth/', include('rest_framework.urls')),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
